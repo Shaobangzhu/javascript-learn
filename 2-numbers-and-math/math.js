db312639cba96d9d2ef6e313fee840405baa501a
Chaoran Lu <andyluloveusc@gmail.com>
2025-11-06 15:42:16 -0800

    learn numbers and basics of js

---
+++ 2-numbers-and-math/math.js
@@ -0,0 +1,9 @@
+// computer cannot store some floats properly
+console.log(20.95 + 7.99); // 28.939999999999998
+
+// do the calculation in integers then convert back to float numbers
+console.log((2095 + 799) / 100); // 28.93
+
+// Round a number
+console.log(Math.round(2.2)); // 2
+console.log(Math.round(2.8)); // 3
